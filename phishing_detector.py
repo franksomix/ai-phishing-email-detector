30098b611a698515c7a65dc5d72ccefaf51645e9
franksomix <franksomix@gmail.com>
2025-06-26 09:57:33 +0100

    Update phishing_detector.py

--- phishing_detector.py
+++ phishing_detector.py
@@ -5,6 +5,6 @@ model = None
 def detect_phishing(text: str) -> str:
     global model
     if model is None:
-        model = pipeline("text-classification", model="bhadresh-savani/bert-base-uncased-emotion")
+        model = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
     result = model(text)[0]
     return result["label"]
